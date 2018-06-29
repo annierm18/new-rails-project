ba857c3c675c2bf4cf5266cba6aca7d0981881e1
Annie Coleman <annierm18@yahoo.com>
2018-06-28 21:14:18 -0700

    checkpoint 3: pt1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery prepend: true
   before_action :authenticate_user!
+  #before_action :configure_permitted_parameters, if: :devise_controller?
 
   helper_method :user_signed_in?
   helper_method :current_user
   helper_method :user_session
+
+  #def configure_permitted_parameters
+  #  devise_parameter_sanitizer.permit(:sign_in) do |user_params|
+    #  user_params.permit(:username, :email)
+  #  end
+#  end
+
+
 end
